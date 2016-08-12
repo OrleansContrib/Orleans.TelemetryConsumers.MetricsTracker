88be6eefdb1668ceefa09cc1cb097b1bb394a69a
Dan <danvanderboom@gmail.com>
2016-08-12 15:20:53 -0600

    fixed up StartSimulation method on SimulatorGrain

--- MetricsTracker.TestHost/TestDomain/SimulatorGrain.cs
+++ MetricsTracker.TestHost/TestDomain/SimulatorGrain.cs
@@ -11,26 +11,43 @@ namespace MetricsTracker.TestHost.TestDomain
     {
         IDisposable SimulationTimer;
 
-        List<Guid> Fleebs = new List<Guid>();
+        List<Guid> Fleebs;
 
-        Random random = new Random();
+        Random random;
 
-        public async Task StartSimulation(TimeSpan duration, int fleebs, int bloords, bool fnord)
+        DateTime StartTime;
+        TimeSpan Duration;
+
+        public Task StartSimulation(TimeSpan duration, int fleebs, int bloords, bool fnord)
         {
             if (SimulationTimer != null)
                 throw new InvalidOperationException("The simulator is already running.");
 
-            Fleebs.Clear();
+            random = new Random();
+
+            StartTime = DateTime.UtcNow;
+            Duration = duration;
+
+            Fleebs = new List<Guid>();
             for (int i = 0; i < 200; i++)
                 Fleebs.Add(Guid.NewGuid());
 
             SimulationTimer = RegisterTimer(RunSimulation, null,
                 dueTime: TimeSpan.Zero,
                 period: TimeSpan.FromSeconds(1));
+
+            return TaskDone.Done;
         }
 
         async Task RunSimulation(object state)
         {
+            // disable the timer when the specified Duration has passed
+            if (DateTime.UtcNow.Subtract(StartTime) >= Duration)
+            {
+                SimulationTimer.Dispose();
+                return;
+            }
+
             // pick between 10 and 50 fleebs
             var activeFleebCount = random.Next(10, 50);
 
