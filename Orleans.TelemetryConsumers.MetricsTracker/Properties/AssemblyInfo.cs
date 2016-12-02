f815cdb66e75230fd8cf7d72d6f33e64bb5e4185
Dan Vanderboom <danvanderboom@gmail.com>
2016-12-01 18:00:06 -0700

    updated all nuget packages to latest versions, including Orleans 1.3.1

--- Orleans.TelemetryConsumers.MetricsTracker/Properties/AssemblyInfo.cs
+++ Orleans.TelemetryConsumers.MetricsTracker/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.13")]
-[assembly: AssemblyFileVersion("0.9.13")]
-[assembly: AssemblyInformationalVersion("0.9.13-beta")]
+[assembly: AssemblyVersion("0.9.15")]
+[assembly: AssemblyFileVersion("0.9.15")]
+[assembly: AssemblyInformationalVersion("0.9.15-beta")]
